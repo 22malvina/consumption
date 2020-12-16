b6af8c3c0db87aae7b032c1fb4462e5522c9c727
root <root@malvina81.fvds.ru>
2020-12-16 21:38:27 +0300

    admin.py

---
+++ cheque/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+
+
+from .models import FNSCheque, FNSChequeElement
+
+#admin.site.register(FNSCheque)
+class FNSChequeElementInline(admin.TabularInline):
+        model = FNSChequeElement
+
+@admin.register(FNSCheque)
+class FNSChequeAdmin(admin.ModelAdmin):
+    #list_display = ('title', 'author', 'display_genre')
+    inlines = [FNSChequeElementInline]
+    search_fields = ['json']
+
+
+#admin.site.register(FNSChequeElement)
+@admin.register(FNSChequeElement)
+class FNSChequeElementAdmin(admin.ModelAdmin):
+    search_fields = ['name', 'price', 'sum', 'quantity']
+    readonly_fields = ['fns_cheque']
+
