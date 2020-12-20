a1c622fbf888048ab240745f72b6db07edae229b
root <root@malvina81.fvds.ru>
2020-12-20 15:57:21 +0300

    try except = save current message and process next message

--- telegram/management/commands/botstart.py
+++ telegram/management/commands/botstart.py
@@ -2,9 +2,24 @@ from django.core.management.base import BaseCommand
 from django.utils import timezone
 from telegram.models import Telegram
 
+import time
+
 class Command(BaseCommand):
     help = 'Displays current time'
 
     def handle(self, *args, **kwargs):
 	while True:
-	    Telegram.process_last_messages_from_bot()
+	    #Telegram.process_last_messages_from_bot()
+            try:
+		Telegram.process_last_messages_from_bot()
+            except:
+                import traceback
+                traceback.print_exc()
+                import sys
+                print sys.exc_info()
+                traceback.print_exception(*sys.exc_info())
+                print '------------sleep-----'
+		time.sleep(15)
+                print '------------'
+
+
